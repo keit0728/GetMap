1f7b7d5d09179e33b3cde0175b676da7e38ca95a
keit0728 <keit0728@gmail.com>
2020-06-01 22:20:51 +0900

    New File

---
+++ GetMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetMap
+//
+//  Created by keit0728 on 2020/06/01.
+//  Copyright © 2020 keit0728. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
